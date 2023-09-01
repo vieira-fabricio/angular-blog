a1405b5878e6467d2a9e3fd6c8d4ddb080da5193
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-08-31 23:38:50 -0300

    criação do component home, e alterações nos componentes menu-tittle e small-car

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuTittleComponent } from './components/menu-tittle/menu-tittle.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BigCardComponent } from './components/big-card/big-card.component';
     MenuBarComponent,
     SmallCardComponent,
     MenuTittleComponent,
-    BigCardComponent
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
